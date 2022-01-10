7d1d908045e20e7a9a312637e62fbb08adbf4944
growingCloud <96115415+growingCloud@users.noreply.github.com>
2022-01-11 08:45:39 +0900

    summernote HTML 편집기를 활용한 게임 작성 및 수정 기능 추가 - 구름

--- 1week_minipj/app.py
+++ 1week_minipj/app.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 from flask import Flask, request, render_template, redirect
-from flask import url_for, flash, session, jsonify, send_from_directory
+from flask import url_for, flash, session, send_from_directory
 from string import digits, ascii_uppercase, ascii_lowercase
 import random
 import os
@@ -10,25 +10,29 @@ app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client.dbsparta_d8
 
-BOARD_IMAGE_PATH = "/Users/mac_cloud/Desktop/images"
+# 이미지 저장 서버경로 및 허용하는 확장자
+# 로컬에서는 절대경로로 "/Users/mac_cloud/Desktop/images" 로 사용함
+BOARD_IMAGE_PATH = " "
 ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg", "gif"])
 
+# 업로드 하는 이미지의 크기 제한, 최대 15MB
 app.config['BOARD_IMAGE_PATH'] = BOARD_IMAGE_PATH
 app.config['MAX_CONTENT_LENGTH'] = 15 * 1024 * 1024
 
+# 만약 서버 저장 경로가 없으면 디렉토리 폴더를 만들어 줌
 if not os.path.exists(app.config['BOARD_IMAGE_PATH']):
     os.mkdir(app.config['BOARD_IMAGE_PATH'])
 
-
+# 파일을 받아올 때 확장자를 검사하는 함수
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1] in ALLOWED_EXTENSIONS
 
-
+# 서버에 사진이 저장되었을 때 임의의 문자+숫자 조합으로 파일명을 변경 해 주는 함수
 def rand_generator(length=8):
     chars = ascii_lowercase + ascii_uppercase + digits
     return ''.join(random.sample(chars, length))
 
-
+# 이미지 업로드 관련 함수, filename을 random.jpg으로 하여 서버에 저장
 @app.route("/upload_image", methods=["POST"])
 def upload_image():
     if request.method == "POST":
@@ -39,10 +43,11 @@ def upload_image():
             file.save(savefilepath)
             return url_for("board_images", filename=filename)
 
-
 @app.route('/images/<filename>')
 def board_images(filename):
     return send_from_directory(app.config['BOARD_IMAGE_PATH'], filename)
+
+
 
 @app.route('/')
 def home():
@@ -72,11 +77,10 @@ def game_create():
 
         idx = db.gameboard.insert_one(post)
 
-
         # SQL의 primary key와 같은 고유 번호(_id) 출력
         return redirect(url_for('game_detail', idx=idx.inserted_id))
     else:
-        # /write 경로로 들어오면 GET으로 받아 입력창 보여줌
+        # /post 경로로 들어오면 GET으로 받아 입력창 보여줌
         return render_template("post.html")
 
 
@@ -86,10 +90,10 @@ def game_edit():
     idx = request.args.get("idx")
 
     if request.method == "GET":
-        data = db.board.find_one({"_id": ObjectId(idx)})
+        data = db.gameboard.find_one({"_id": ObjectId(idx)})
         if data is None:
             flash("해당 게시물이 존재하지 않습니다.")
-            return redirect(url_for("board_lists"))
+            return redirect(url_for("game_lists"))
         else:
             if session.get("id") == data.get("writer_id"):
                 return render_template("edit.html", data=data)
@@ -97,59 +101,41 @@ def game_edit():
                 flash("글 수정 권한이 없습니다.")
                 return redirect(url_for("board_lists"))
     else:
-        title = request.form.get("title")
+        img_title_left = request.form.get("img_title_left"),
+        img_title_right = request.form.get("img_title_left"),
+        img_url_left = request.form.get("img_url_left"),
+        img_url_right = request.form.get("img_url_right"),
         contents = request.form.get("contents")
 
-        data = db.board.find_one({"_id": ObjectId(idx)})
+        data = db.gameboard.find_one({"_id": ObjectId(idx)})
 
         if data.get("writer_id") == session.get("id"):
-            db.board.update_one({"_id": ObjectId(idx)}, {
+            db.gameboard.update_one({"_id": ObjectId(idx)}, {
                 "$set": {
-                    "title": title,
+                    "img_title_left": img_title_left,
+                    "img_title_right": img_title_right,
+                    "img_url_left": img_url_left,
+                    "img_url_right": img_url_right,
                     "contents": contents,
                 }
             })
             flash("수정되었습니다.")
-            return redirect(url_for("board_view", idx=idx))
+            return redirect(url_for("game_view", idx=idx))
         else:
             flash("글 수정 권한이 없습니다.")
-            return redirect(url_for("board_lists"))
+            return redirect(url_for("game_lists"))
 
 
 # 게시글 삭제 기능 추가 (Delete)
 @app.route("/api/detail", methods=["DELETE"])
 def game_delete():
     idx = request.args.get("idx")
-    data = db.board.find_one({"_id": ObjectId(idx)})
+    data = db.gameboard.find_one({"_id": ObjectId(idx)})
     if data.get("writer_id") == session.get("id"):
-        db.board.delete_one({"_id": ObjectId(idx)})
-        flash("삭제 되었습니다.")
+        db.gameboard.delete_one({"_id": ObjectId(idx)})
     else:
         flash("글 삭제 권한이 없습니다.")
-    return redirect(url_for("board_lists"))
-
-
-# 게시글 상세 페이지 (Read)
-@app.route('/view', methods=['GET'])
-def board_view():
-    idx = request.args.get("idx")
-
-    if idx is not None:
-        data = db.board.find_one({"_id": ObjectId(idx)})
-        if data is not None:
-            result = {
-                "id": data.get("_id"),
-                "name": data.get("name"),
-                "title": data.get("title"),
-                "contents": data.get("contents"),
-                "pubdate": data.get("pubdate"),
-                "writer_id": data.get("writer_id", "")
-            }
-
-            comments = db.c_comment.find({"root_idx": str(data.get("_id"))})
-
-            return render_template("view.html", comments=list(comments), result=result)
-
+    return redirect(url_for("game_lists"))
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5003, debug=True)
+    app.run('0.0.0.0', port=5004, debug=True)
